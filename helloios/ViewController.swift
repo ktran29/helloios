015c529d2f98b91aff4b9c47d9c02767bfb6d337
Kevin Tran <ktran@tack.tech>
2018-01-11 17:16:25 -0800

    Initial Commit

---
+++ helloios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  helloios
+//
+//  Created by Kevin Tran on 1/11/18.
+//  Copyright © 2018 Kevin Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
